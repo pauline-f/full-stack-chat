edfdb282cfbc77ff962a4d16684cc9f728b5b625
pauline <pauline.feytel@gmail.com>
2020-01-15 10:53:32 +0100

    Create login component and add login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
+import { Route, BrowserRouter } from "react-router-dom";
 import './App.css';
 import MessageForm from './MessageForm';
 import ListMessages from './ListMessages';
+import Login from './Login';
 
 function App() {
 
   const [messages, setMessages] = useState([]);
-  
+
 
   return (
     <div className="App">
       <ListMessages messages={messages} />
       <MessageForm messages={messages} setMessages={setMessages} />
+
+      <BrowserRouter>
+        <Route path='/login' render={() => <Login component={Login} />} />
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ client/src/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Login = () => {
+  
+  return (
+    <form>
+        Nickname: <input type='text' />
+        <input type='submit' value='Connect' />
+      </form>
+  );
+};
+
+export default Login;
